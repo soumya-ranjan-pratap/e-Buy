2d1c0ed648e55d72c308ab43167767cdc4245455
Soumya Ranjan Pratap <srpratap021@gmail.com>
2023-09-11 17:26:26 +0530

    Some changes have made

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,8 @@ namespace e_Buy.app.Controllers
             return View();
         }
 
+        
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace e_Buy.app.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
